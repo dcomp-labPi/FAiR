686f9c987d7457f5afee9780d5871e4c590642e6
diego-carvalho <diego.a.carvalho@hotmailcom>
2017-03-27 14:41:18 -0300

    Add install file

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# install python
+sudo apt-get update
+sudo apt-get install python
+
+# install numpy library
+sudo apt-get install python-numpy  
+
+# install matplotlib library
+sudo apt-get install python-matplotlib
+
+# install seaborn library
+sudo apt-get install python-seaborn
