eb8ddb04d4609e47012b8784d617c36cf1722a51
Diego <diego.a.carvalho@hotmail.com>
2017-03-29 22:17:33 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@ sudo apt-get update
 sudo apt-get install python3
 
 # install numpy library
-sudo apt-get install python-numpy  
+sudo apt-get install python3-numpy  
 
 # install matplotlib library
-sudo apt-get install python-matplotlib
+sudo apt-get install python3-matplotlib
 
 # install seaborn library
-sudo apt-get install python-seaborn
+sudo apt-get install python3-seaborn
